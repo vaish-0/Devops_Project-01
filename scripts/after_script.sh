723663ab7e9a503fb009cbae8daba053ed6177e9
vaish-0 <98145252+vaish-0@users.noreply.github.com>
2025-07-13 23:38:55 +0530

    Update after_script.sh

--- scripts/after_script.sh
+++ scripts/after_script.sh
@@ -2,4 +2,4 @@
 
 sudo -i
 
-docker container run -d --name apache -p 80:80 987240292938.dkr.ecr.ap-south-1.amazonaws.com/http:latest
+docker container run -d --name apache -p 80:80 120569649088.dkr.ecr.ap-south-1.amazonaws.com/dev/new
